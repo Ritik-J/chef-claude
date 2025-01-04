ab11b34ffdd1ae05f0b5532622eb1d99cdaee759
Ritik <jainritik135@gmail.com>
2025-01-04 17:08:38 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex justify-center items-center h-28 gap-3 shadow-md shadow-#0000000F rounded-md">
+      <img src="/icon.png" alt="logo" />
+      <h1 className="text-3xl">Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
